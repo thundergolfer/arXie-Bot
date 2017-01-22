ba0ce9fc329e65dbb5318e89c7abef6d00769d6d
Jono Belotti <jbelotti@zendesk.com>
2017-01-22 12:51:27 +1100

    removing credentials files

--- bot/private_settings.py
+++
@@ -1,6 +0,0 @@
-
-apiai_access_token = '952f1206625a441e97e853262ff940fa'
-
-slack_token = 'xoxb-69409833526-w5EUwZaYaS2hhNRwrjXP5Pa0'
-
-beepboop_token = ''
--- private_settings.js
+++
@@ -1,2 +0,0 @@
-
-api_token = 'xoxb-69409833526-G4ws0lJEQmYQzfI8yUboytDK';
