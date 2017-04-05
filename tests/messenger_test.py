883ebf706c02744de0c5badc30f3b7f91b45b5ba
Jonathon Belotti <jbelotti@zendesk.com>
2017-04-05 20:22:21 +1000

    first test passed

--- tests/messenger_test.py
+++ tests/messenger_test.py
@@ -18,4 +18,9 @@ class TestMessenger():
             mock_ch_id = 1000
             messenger.write_help_message(1000)
 
-        mock_messenger.assert_called_with(1000, "")
+        expected_message =  str("I'm your friendly Slack bot written in Python.  I'll *_respond_* to the following commands:" +
+                            "\n> `hi <@mr. mockbot>` - I'll respond with a randomized greeting mentioning your user. :wave:" +
+                            "\n> `<@mr. mockbot> joke` - I'll tell you one of my finest jokes, with a typing pause for effect. :laughing:" +
+                            "\n> `<@mr. mockbot> attachment` - I'll demo a post with an attachment using the Web API. :paperclip:")
+
+        mock_messenger.assert_called_with(1000, expected_message)
