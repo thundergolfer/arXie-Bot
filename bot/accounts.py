c90e2d7eb2403b109fe4c4a252593dd15319121d
Jonathon Belotti <jbelotti@zendesk.com>
2017-05-10 21:03:13 +1000

    need to base64 decode to store with json

--- bot/accounts.py
+++ bot/accounts.py
@@ -1,9 +1,11 @@
 import json
 import os
+import base64
 from bot.crypt import encrypt, decrypt
 
 LOGIN_DB_FILENAME = "logins.json"
 LOGIN_DB = os.path.join(os.path.dirname(__file__), LOGIN_DB_FILENAME)
+
 
 def load_db():
     logins = {}
@@ -26,9 +28,9 @@ def update_with_user(team, slack_user, username, pw):
     logins = load_db()
     if team not in logins:
         logins[team] = {}
-    if slack_user not in logins:
+    if slack_user not in logins[team]:
         logins[team][slack_user] = {}
-        logins[team][slack_user]['username'] = encrypt(pw)
+        logins[team][slack_user]['username'] = base64.encodestring(encrypt(pw))
         logins[team][slack_user]['password'] = pw
         with open(LOGIN_DB, 'w') as fp:
             json.dump(logins, fp)
@@ -39,7 +41,7 @@ def add_user(team, slack_user, username, pw):
     if team not in logins:
         logins[team] = {}
     logins[team][slack_user]['username'] = username
-    logins[team][slack_user]['password'] = encrypt(pw)
+    logins[team][slack_user]['password'] = base64.encodestring(encrypt(pw))
     with open(LOGIN_DB, 'w') as fp:
         json.dump(logins, fp)
 
@@ -47,7 +49,7 @@ def get_user(team, slack_user):
     logins = load_db()
     if team in logins and slack_user in logins[team]:
         user = logins[team][slack_user]['username']
-        pw = decrypt(logins[team][slack_user]['password'])
+        pw = decrypt(base64.decodestring(logins[team][slack_user]['password']))
         return user, pw
 
     return None, None
